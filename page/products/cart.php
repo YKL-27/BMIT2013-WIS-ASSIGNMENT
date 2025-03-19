2ccb25418c1efe188cf884e8ee3a6c2fa06feb28
shengli250 <kelvinsoh250@gmail.com>
2025-03-19 16:28:21 +0800

    update cart.php based on _base.php
    fixed the picture cant show in browser and change little bit php in cart.php

--- page/products/cart.php
+++ page/products/cart.php
@@ -6,29 +6,34 @@ require '../../_base.php';
 $_title = 'MY CART';
 include '../../_head.php';
 $is_logged_in = isset($_SESSION['user']);
+$cart = get_cart();
 ?>
 
 <div class="cart-container">
-    <img src="image/cartPicture.jpg">
-    <h1>YOUR CART IS EMPTY</h1>
+    <?php if (!empty($cart)): ?>
 
-    <?php 
-    if ($is_logged_in) {
-        echo "<p>Oops! You haven’t added an Epal Aifon yet. Check out the newest series and pick your favorite!</p>";
-    } else {
-        echo "<p>Login to your Epal account to view your saved items or continue shopping</p>";
-    }
-    ?>
-
-    <div class="cartEmpty-buttons">
-        <a href="../../index.php">Continue Shopping</a>
+    <?php else: ?>
+        <img src="<?= BASE_URL ?>image/cartPicture.png" style="width: 100px; height: auto;">
+        <h1>YOUR CART IS EMPTY</h1>
 
         <?php 
-            if (!$is_logged_in) {
-                echo '<a href="../member/login.php">Login</a>';
-            }
+        if ($is_logged_in) {
+            echo "<p>Oops! You haven’t added an Epal Aifon yet. Check out the newest series and pick your favorite!</p>";
+        } else {
+            echo "<p>Login to your Epal account to view your saved items or continue shopping</p>";
+        }
         ?>
-    </div>
+
+        <div class="cartEmpty-buttons">
+            <a href="../../index.php">Continue Shopping</a>
+
+            <?php 
+                if (!$is_logged_in) {
+                    echo '<a href="../member/login.php">Login</a>';
+                }
+            ?>
+        </div>
+    <?php endif; ?>
 </div>
 
 <?php
